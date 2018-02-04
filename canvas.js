f262ee759829af71d560dcde14bbbd432ffb1e6f
Max Korsun <mkorsun@stuy.edu>
2018-02-04 16:36:31 -0500

    pushed changes

--- canvas.js
+++ canvas.js
@@ -6,5 +6,18 @@ var clear = function() {
     ctx.clearRect(0,0,500,500);
 }
 
-var cbutton = document.getLementById("clear")
-cbutton.addEventListenter("click", clear)
+var cbutton = document.getElementById("clear");
+cbutton.addEventListener("click", clear);
+
+var tbutton = document.getElementById("toggle");
+tbutton.addEventListener("click",toggle);
+
+var circle = function(e) {
+    ctx.beginPath();
+    ctx.arc(e.ClientX, e.ClientY, 25, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+    console.log("asdf");
+}
+
+c.addEventListener("click", circle);
